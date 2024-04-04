41f4c16e4c987694ca4ac9c0bec9fd9b218e1bcd
mauroloon <mauricio.quezadagarcia@gmail.com>
2024-04-03 23:50:26 -0300

    Add new Python scripts for string manipulation

---
+++ 2_strings/case_personal_message.py
@@ -0,0 +1,4 @@
+name = 'Mauricio'
+message = f'Hello {name}, would you like to learn some Python today?'
+
+print(message)
---
+++ 2_strings/celebrity_phrase.py
@@ -0,0 +1,6 @@
+
+author = 'Albert Einstein'
+message = "La locura es hacer lo mismo una y otra vez esperando obtener 'resultados' diferentes."
+
+complete_message = f'{message} \n{author}'
+print(complete_message)
